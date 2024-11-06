abd44062136682fa7f11c4009a71b9a7531b9498
SIMJIYEON93 <930515s@gmail.com>
2024-11-06 19:35:50 +0900

    feat: security-jwt

---
+++ src/main/java/com/example/oauthlogin/security/JwtUtil.java
@@ -0,0 +1,106 @@
+package com.example.oauthlogin.security;
+
+import com.example.oauthlogin.entity.UserRoleEnum;
+import com.example.oauthlogin.repository.BlacklistedTokenRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Slf4j(topic = "JwtUtil")
+@Component
+@Getter
+@RequiredArgsConstructor
+public class JwtUtil {
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String AUTHORIZATION_KEY = "auth";
+    public static final String BEARER_PREFIX = "Bearer ";
+    public static final long ACCESS_TOKEN_TIME = 60 * 60 * 1000L * 3; // 3시간
+    public static final long REFRESH_TOKEN_TIME = 60 * 60 * 1000L * 24 * 3; // 3일
+
+    @Value("${jwt.secretkey}")
+    private String secretKey;
+
+    private Key key;
+
+    private final BlacklistedTokenRepository blacklistedTokenRepository;
+
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+    @PostConstruct
+    public void init() {
+        byte[] bytes = Base64.getDecoder().decode(secretKey);
+        key = Keys.hmacShaKeyFor(bytes);
+    }
+
+    public String extractTokenFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    public String createAccessToken(String username, UserRoleEnum role) {
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setSubject(username)
+                .claim(AUTHORIZATION_KEY, role)
+                .setExpiration(new Date(now.getTime() + ACCESS_TOKEN_TIME))
+                .setIssuedAt(now)
+                .signWith(key, signatureAlgorithm)
+                .compact();
+    }
+
+    public String createRefreshToken() {
+        Date now = new Date();
+
+        return Jwts.builder()
+                .setClaims(Jwts.claims())
+                .setExpiration(new Date(now.getTime() + REFRESH_TOKEN_TIME))
+                .setIssuedAt(now)
+                .signWith(key, signatureAlgorithm)
+                .compact();
+    }
+
+    public boolean isValidToken(String token) {
+        try {
+            getClaims(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public boolean isTokenBlacklisted(String token) {
+        return blacklistedTokenRepository.existsById(token);
+    }
+
+    public long getRemainingTime(String token) {
+        Claims claims = getClaims(token);
+        Date expiration = claims.getExpiration();
+        return expiration.getTime() - System.currentTimeMillis();
+    }
+
+    public Claims getClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
+
---
+++ src/main/java/com/example/oauthlogin/security/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.example.oauthlogin.security;
+
+import com.example.oauthlogin.entity.User;
+import com.example.oauthlogin.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetails loadUserById(Long id){
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저가 존재하지 않습니다."));
+        return new UserDetailsImpl(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Not Found " + email));
+
+        return new UserDetailsImpl(user);
+    }
+
+
+}
